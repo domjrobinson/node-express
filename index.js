c962086c4dc55c45aa4fae512ed7a443111cfcd4
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-21 21:34:30 -0500

    feat/render-handlebars-view-toclient

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 
 const app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Home route
+app.get('/', (req, res) => res.render('index'));
+
 app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
