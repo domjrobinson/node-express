2e5346b6168d35b0f7bd2e4476a693325056d262
Dominique Robinson <domrobinson.dj@gmail.com>
2019-04-21 21:16:57 -0500

    feat/get-member-by-id-route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const logger = require('./middleware/logger');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
 const members = require('../../Members');
 
+// get member by id
 router.get('/:id', (req, res) => {
   const found = members.some(member => member.id === parseInt(req.params.id));
 
